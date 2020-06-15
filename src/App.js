25c90e4d88ff6451df984858ecdfb62a255ffa4c
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-06-14 21:40:36 -0400

    Add navbar and footer to app and remove an unneeded path

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <Router>
       <div>
+        <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/home" component={Home} />
           <Route exact path="/portfolio" component={Portfolio} />
           <Route exact path="/contact" component={Contact} />
         </Switch>
+        <Footer />
       </div>
     </Router>
   );
