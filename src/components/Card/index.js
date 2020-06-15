4d6747d2bd59eaafbcf22e585c87c60435b48e08
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-06-15 14:20:08 -0400

    Add cards for the portfolio with information

---
+++ src/components/Card/index.js
@@ -0,0 +1,103 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <div className="row">
+        <div className="card-deck p-3">
+          <div className="card mb-3">
+            <img src="./assets/images/2020:02:16-EventBook-Screenshot.png" className="card-img-top" alt="portfolio" />
+            <div className="card-body">
+              <h3 className="card-title">EventBook</h3>
+              <h4>Links:</h4>
+              <p>Repository: <a href="https://github.com/AJFree458/Project_opus_1">github.com/AJFree458/Project_opus_1</a></p>
+              <p>Deployment: <a href="https://ajfree458.github.io/Project_opus_1/">ajfree458.github.io/Project_opus_1/</a></p>
+            </div>
+          </div>
+          <div className="card mb-3">
+            <img src="./assets/images/Project-2_Demo.png" className="card-img-top" alt="portfolio" />
+            <div className="card-body">
+              <h3 className="card-title">Project-2_Volunteer</h3>
+              <h4>Links:</h4>
+              <p>Repository: <a href="https://github.com/AJFree458/Project-2">github.com/AJFree458/Project-2</a></p>
+              <p>Deployment: <a href="https://project2-volunteer.herokuapp.com/">project2-volunteer.herokuapp.com/</a></p>
+            </div>
+          </div>
+          <div className="card mb-3">
+            <img src="./assets/images/2020:03:16-PasswordGen-Screenshot.png" className="card-img-top" alt="portfolio" />
+            <div className="card-body">
+              <h3 className="card-title">Password Generator</h3>
+              <h4>Links:</h4>
+              <p>Repository: <a href="https://github.com/AJFree458/opus3_pgenerator">github.com/AJFree458/opus3_pgenerator</a></p>
+              <p>Deployment: <a href="https://ajfree458.github.io/opus3_pgenerator/">ajfree458.github.io/opus3_pgenerator/</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="card-deck p-3">
+          <div className="card mb-3">
+            <img src="./assets/images/2020:03:16-JavascriptQuiz-Screenshot.png" className="card-img-top" alt="portfolio" />
+            <div className="card-body">
+              <h3 className="card-title">JS Quiz</h3>
+              <h4>Links:</h4>
+              <p>Repository: <a href="https://github.com/AJFree458/opus4_js_basics_quiz">github.com/AJFree458/opus4_js_basics_quiz</a></p>
+              <p>Deployment: <a href="https://ajfree458.github.io/opus4_js_basics_quiz/">ajfree458.github.io/opus4_js_basics_quiz/</a></p>
+            </div>
+          </div>
+          <div className="card mb-3">
+            <img src="./assets/images/Burger_Demo.png" className="card-img-top" alt="portfolio" />
+            <div className="card-body">
+              <h3 className="card-title">MVC Eat-Da Burger</h3>
+              <h4>Links:</h4>
+              <p>Repository: <a href="https://github.com/AJFree458/opus13_MVC-Burger">github.com/AJFree458/opus13_MVC-Burger</a></p>
+              <p>Deployment: <a href="https://warm-plains-37625.herokuapp.com/">Heroku App</a></p>
+            </div>
+          </div>
+          <div className="card mb-3">
+            <img src="./assets/images/2020:03:16-WeatherDashboard-Screenshot.png" className="card-img-top" alt="portfolio" />
+            <div className="card-body">
+              <h3 className="card-title">Weather Dashboard</h3>
+              <h4>Links:</h4>
+              <p>Repository: <a href="https://github.com/AJFree458/opus6_weather_dash">github.com/AJFree458/opus6_weather_dash</a></p>
+              <p>Deployment: <a href="https://ajfree458.github.io/opus6_weather_dash/">ajfree458.github.io/opus6_weather_dash/</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="card-deck p-3">
+          <div className="card mb-3">
+            <img src="./assets/images/Note-Taker_Demo.png" className="card-img-top" alt="portfolio" />
+            <div className="card-body">
+              <h3 className="card-title">Express Note Taker</h3>
+              <h4>Links:</h4>
+              <p>Repository: <a href="https://github.com/AJFree458/opus11_express-note-taker">github.com/AJFree458/opus11_express-note-taker</a></p>
+              <p>Deployment: <a href="https://ancient-woodland-06582.herokuapp.com/">ancient-woodland-06582.herokuapp.com/</a></p>
+            </div>
+          </div>
+          <div className="card mb-3">
+            <img src="./assets/images/Employee-Tracker_Demo.png" className="card-img-top" alt="portfolio" />
+            <div className="card-body">
+              <h3 className="card-title">Employee Tracker</h3>
+              <h4>Links:</h4>
+              <p>Repository: <a href="https://github.com/AJFree458/opus12_employee-tracker">github.com/AJFree458/opus12_employee-tracker</a></p>
+              <p>Deployment: N/A</p>
+            </div>
+          </div>
+          <div className="card mb-3">
+            <img src="./assets/images/Readme-generator_Demo.png" className="card-img-top" alt="portfolio" />
+            <div className="card-body">
+              <h3 className="card-title">Readme Generator</h3>
+              <h4>Links:</h4>
+              <p>Repository: <a href="https://github.com/AJFree458/opus9_readme-generator">github.com/AJFree458/opus9_readme-generator</a></p>
+              <p>Deployment: N/A</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
